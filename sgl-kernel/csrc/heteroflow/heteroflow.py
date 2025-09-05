b569ae57b14431cd58a832497d74529934ece68f
Zheng, Beilei <beilei.zheng@intel.com>
2025-09-05 08:58:56 +0000

    init fused_moe

--- sgl-kernel/csrc/heteroflow/heteroflow.py
+++ sgl-kernel/csrc/heteroflow/heteroflow.py
@@ -26,11 +26,90 @@ heteroflow_cpp = load(
     extra_ldflags=["-L/usr/local/cuda/lib64", "-lcudart"],
 )
 import torch
-from heteroflow_cpp import CPUInfer, submit_simple_add_one
+from heteroflow_cpp import CPUInfer, fused_moe_cpu
+import math
 
-x = torch.tensor([[1.0, 2.0],[3.0,4.0]]).to('cpu')
+
+def scaled_weight(weight, scales):
+    E, N, K = weight.shape
+    pad_N = (BLOCK_N - (N % BLOCK_N)) % BLOCK_N
+    pad_K = (BLOCK_K - (K % BLOCK_K)) % BLOCK_K
+
+    if pad_N > 0 or pad_K > 0:
+        weight = torch.nn.functional.pad(weight, (0, pad_K, 0, pad_N))
+
+    weight_block = (
+        weight.view(E, math.ceil(N / BLOCK_N), BLOCK_N, math.ceil(K / BLOCK_K), BLOCK_K)
+        .permute(0, 1, 3, 2, 4)
+        .float()
+        .contiguous()
+    )
+
+    weight_scaled = (
+        (
+            weight_block
+            * scales.view(E, math.ceil(N / BLOCK_N), math.ceil(K / BLOCK_K), 1, 1)
+        )
+        .permute(0, 1, 3, 2, 4)
+        .contiguous()
+    )
+    if pad_N > 0 or pad_K > 0:
+        weight_scaled = weight_scaled.view(E, N + pad_N, K + pad_K)
+        weight_scaled = weight_scaled[..., :N, :K].contiguous()
+    else:
+        weight_scaled = weight_scaled.view(E, N, K)
+    return weight_scaled
+from sgl_kernel_cpu import common_ops
+
+M = 2
+N = 512
+K = 256
+E = 8
+topk = 4
+dtype = torch.bfloat16
+BLOCK_N, BLOCK_K = 64, 128
+factor_for_scale = 1e-3
+fp8_max, fp8_min = 400, -400
+
+a = torch.randn(M, K, dtype=dtype) / math.sqrt(K)
+w1_fp32 = torch.randn(E, 2 * N, K)
+w1 = (w1_fp32 * fp8_max).clamp(min=fp8_min, max=fp8_max).to(torch.float8_e4m3fn)
+w2_fp32 = torch.randn(E, K, N)
+w2 = (w2_fp32 * fp8_max).clamp(min=fp8_min, max=fp8_max).to(torch.float8_e4m3fn)
+w1s = (
+    torch.randn(E, math.ceil(2 * N / BLOCK_N), math.ceil(K / BLOCK_K))
+    * factor_for_scale
+)
+w2s = (
+    torch.randn(E, math.ceil(K / BLOCK_N), math.ceil(N / BLOCK_K))
+    * factor_for_scale
+)
+
+w1_scaled = scaled_weight(w1, w1s)
+w2_scaled = scaled_weight(w2, w2s)
+
+score = torch.randn((M, E), dtype=dtype)
+score = torch.softmax(score, dim=-1, dtype=torch.float32)
+topk_weight, topk_ids = torch.topk(score, topk)
+
+w1 = torch.ops.sgl_kernel.convert_weight_packed(w1)
+w2 = torch.ops.sgl_kernel.convert_weight_packed(w2)
+
 
 cpuinfer = CPUInfer(20)
-cpuinfer.submit_with_cuda_stream(torch.cuda.current_stream().cuda_stream, submit_simple_add_one(x))
+cpuinfer.submit_with_cuda_stream(torch.cuda.current_stream().cuda_stream, fused_moe_cpu(a,
+            w1,
+            w2,
+            topk_weight,
+            topk_ids.to(torch.int32),
+            False,
+            False,
+            True,
+            w1s,
+            w2s,
+            [BLOCK_N, BLOCK_K],
+            None,
+            None,
+            True,))
 
 cpuinfer.sync_with_cuda_stream(torch.cuda.current_stream().cuda_stream)
--- sgl-kernel/csrc/heteroflow/heteroflow_bindings.cpp
+++ sgl-kernel/csrc/heteroflow/heteroflow_bindings.cpp
@@ -102,27 +102,73 @@ using namespace pybind11::literals;
 
 
 
-at::Tensor simple_add_one(at::Tensor& input) {
-    return input + 1;
-}
+at::Tensor fused_experts_cpu2(
+    at::Tensor& hidden_states,
+    at::Tensor& w1,
+    at::Tensor& w2,
+    at::Tensor& topk_weights,
+    at::Tensor& topk_ids,
+    bool inplace,
+    bool use_int8_w8a8,
+    bool use_fp8_w8a16,
+    const std::optional<at::Tensor>& w1_scale,
+    const std::optional<at::Tensor>& w2_scale,
+    const std::optional<std::vector<int64_t>> block_size,
+    const std::optional<at::Tensor>& a1_scale,
+    const std::optional<at::Tensor>& a2_scale,
+    bool is_vnni) {
+        return hidden_states;
+    }
 
 class SimpleBindings {
 public:
     struct Args {
         CPUInfer* cpuinfer;
-        at::Tensor input;
+        at::Tensor hidden_states;
+        at::Tensor w1;
+        at::Tensor w2;
+        at::Tensor topk_weights;
+        at::Tensor topk_ids;
+        bool inplace;
+        bool use_int8_w8a8;
+        bool use_fp8_w8a16;
+        std::optional<at::Tensor> w1_scale;
+        std::optional<at::Tensor> w2_scale;
+        std::optional<std::vector<int64_t>> block_size;
+        std::optional<at::Tensor> a1_scale;
+        std::optional<at::Tensor> a2_scale;
+        bool is_vnni;
         at::Tensor output;
     };
 
     static void inner(void* vargs) {
         Args* args = (Args*)vargs;
-        at::Tensor out = simple_add_one(args->input);
+        at::Tensor out = fused_experts_cpu2(
+            args->hidden_states,
+            args->w1,
+            args->w2,
+            args->topk_weights,
+            args->topk_ids,
+            args->inplace,
+            args->use_int8_w8a8,
+            args->use_fp8_w8a16,
+            args->w1_scale,
+            args->w2_scale,
+            args->block_size,
+            args->a1_scale,
+            args->a2_scale,
+            args->is_vnni
+        );
         args->output.copy_(out);
     }
 
-    static std::pair<intptr_t,intptr_t> cpuinfer_interface(const at::Tensor& input) {
-        at::Tensor output = at::empty_like(input);
-        Args* args = new Args{nullptr, input, output};
+    static std::pair<intptr_t,intptr_t> cpuinfer_interface(at::Tensor& hidden_states, at::Tensor& w1, at::Tensor& w2, at::Tensor& topk_weights, at::Tensor& topk_ids,
+        bool inplace, bool use_int8_w8a8, bool use_fp8_w8a16, std::optional<at::Tensor> w1_scale,
+        std::optional<at::Tensor> w2_scale, std::optional<std::vector<int64_t>> block_size,
+        std::optional<at::Tensor> a1_scale,
+        std::optional<at::Tensor> a2_scale, bool is_vnni) {
+        at::Tensor output = at::empty_like(hidden_states);
+        Args* args = new Args{nullptr, hidden_states, w1, w2, topk_weights, topk_ids, inplace, use_int8_w8a8, use_fp8_w8a16, w1_scale, w2_scale, block_size, a1_scale, a2_scale, is_vnni, output};
         return { (intptr_t)&inner, (intptr_t)args };
     }
 };
@@ -136,5 +182,5 @@ PYBIND11_MODULE(heteroflow_cpp, m) {
         .def("sync", &CPUInfer::sync)
         .def("sync_with_cuda_stream", &CPUInfer::sync_with_cuda_stream);
 
-    m.def("submit_simple_add_one", &SimpleBindings::cpuinfer_interface);
+    m.def("fused_moe_cpu", &SimpleBindings::cpuinfer_interface);
 }
