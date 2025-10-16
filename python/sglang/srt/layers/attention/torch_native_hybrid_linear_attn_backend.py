1364a8780d388a3c39be1c2a416176b0bc505d8b
Zheng, Beilei <beilei.zheng@intel.com>
2025-10-16 01:07:16 -0700

    remove transpose overhead

--- python/sglang/srt/layers/attention/torch_native_hybrid_linear_attn_backend.py
+++ python/sglang/srt/layers/attention/torch_native_hybrid_linear_attn_backend.py
@@ -400,7 +400,7 @@ class MambaAttnBackend(AttentionBackend):
         conv_states, ssm_states = self.req_to_token_pool.get_mamba_params(layer_id)
         cache_indices = self.forward_metadata.mamba_cache_indices
         mixed_qkv = torch.ops.sgl_kernel.causal_conv1d_update_cpu(
-            mixed_qkv.unsqueeze(1).transpose(1,2),
+            mixed_qkv,
             conv_states,
             cache_indices,
             conv_weights,
@@ -408,15 +408,6 @@ class MambaAttnBackend(AttentionBackend):
             activation=="silu",
             None,
         )
-        # mixed_qkv, new_conv_states = torch_causal_conv1d_update(
-        #     mixed_qkv.unsqueeze(1).transpose(1,2),
-        #     conv_states[cache_indices],
-        #     conv_weights,
-        #     bias,
-        #     activation=="silu",
-        # )
-
-        mixed_qkv = mixed_qkv.squeeze(-1)
 
         query, key, value = torch.split(
             mixed_qkv,
