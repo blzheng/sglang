1d9a31a92c33f4a399a752203d37cb5607badf9c
Zheng, Beilei <beilei.zheng@intel.com>
2026-01-03 22:23:31 -0800

    apply optimized conv3d

--- python/sglang/srt/models/qwen3_vl.py
+++ python/sglang/srt/models/qwen3_vl.py
@@ -29,6 +29,7 @@ from transformers.models.qwen2_5_vl.modeling_qwen2_5_vl import (
 
 from sglang.srt.configs.qwen3_vl import Qwen3VLConfig, Qwen3VLVisionConfig
 from sglang.srt.layers.attention.vision import VisionAttention
+from sglang.srt.layers.conv import Conv3d
 from sglang.srt.layers.linear import ColumnParallelLinear, RowParallelLinear
 from sglang.srt.layers.logits_processor import LogitsProcessor
 from sglang.srt.layers.pooler import Pooler, PoolingType
@@ -99,7 +100,7 @@ class Qwen3VLVisionPatchEmbed(nn.Module):
         self.embed_dim = config.hidden_size
 
         kernel_size = [self.temporal_patch_size, self.patch_size, self.patch_size]
-        self.proj = nn.Conv3d(
+        self.proj = Conv3d(
             self.in_channels,
             self.embed_dim,
             kernel_size=kernel_size,
